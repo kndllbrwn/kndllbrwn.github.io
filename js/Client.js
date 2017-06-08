0df9a20d45e1f5594c3e94079ab9f6136659d0f2
kndllbrwn <kndllbrwn1914@gmail.com>
2017-06-07 21:22:33 -0400

    Added react component to page

--- js/Client.js
+++ js/Client.js
@@ -1,1 +1,12 @@
-console.log('hiiii');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Component extends React.Component{
+  render() {
+    return (
+      <h1> React component</h1>
+    )
+  }
+}
+
+ReactDom.render(<Component/>, document.getElementById('react'));
