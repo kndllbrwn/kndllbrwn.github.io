6ce333f9fdeb7d82eb51b4d0584abf569fcfcb06
kndllbrwn <kndllbrwn1914@gmail.com>
2017-06-21 22:34:08 -0400

    Commented code to be used later and bundled

--- js/components/Position.jsx
+++ js/components/Position.jsx
@@ -6,10 +6,10 @@ const Position = React.createClass({
     return (
       <ul className='position'>
         <h3>{title}</h3>
-        <details>
+        {/*<details>
           <summary>Desc</summary>
           fsdfdfdf
-        </details>
+        </details>*/}
         <p><i><a href={link} target='_blank'>{company}</a></i>                                                                  <span>{year}</span></p>
         {description.map((role) => { return <li>{role}</li> })}
       </ul>
