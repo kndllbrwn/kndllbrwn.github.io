92bcb1a532fc38d840c425cb9ff8e4e510a6e38e
kndllbrwn <kndllbrwn1914@gmail.com>
2017-07-04 15:19:21 -0400

    Incorporated flipcard successfully

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import preload from '../public/jobs.json'
 import { Grid, Row, Col } from 'react-flexbox-grid'
+import FlipMove from 'react-flip-move'
+import FlipCard from 'react-flipcard'
 
 import '../public/style.css'
 
@@ -14,10 +16,37 @@ import Examples from './components/Examples'
 import Experience from './components/Experience'
 import Interests from './components/Interests'
 import Skills from './components/Skills'
-
-
-
+ 
 const App = React.createClass({
+  getInitialState() {
+    return {
+      isFlipped: false
+    };
+  },
+
+  showBack() {
+    this.setState({
+      isFlipped: true
+    });
+  },
+
+  showFront() {
+    this.setState({
+      isFlipped: false
+    });
+  },
+
+  handleOnFlip(flipped) {
+    if (flipped) {
+      this.refs.backButton.getDOMNode().focus();
+    }
+  },
+
+  handleKeyDown(e) {
+    if (this.state.isFlipped && e.keyCode === 27) {
+      this.showFront();
+    }
+  },
   render () {
     return (
       <Grid fluid>
@@ -31,9 +60,25 @@ const App = React.createClass({
         <div className='flex-container'>
           <Row>
             <Col xs={9}>
-              <Carousel />
-              {/*<Experience jobs={preload.jobs} />
+              {/*{<Carousel />
+              <Experience jobs={preload.jobs} />
               <Examples projects={preload.projects} />*/}
+              <FlipCard
+                disabled={true}
+                flipped={this.state.isFlipped}
+                onFlip={this.handleOnFlip}
+                onKeyDown={this.handleKeyDown}
+              >
+                <div>
+                  
+                  <button type="button" onClick={this.showBack}>Examples</button>
+                   <Experience jobs={preload.jobs} /> 
+                </div>
+                <div>
+                  <button type="button" ref="backButton" onClick={this.showFront}>Experience</button>
+                  <Examples projects={preload.projects} />
+                </div>
+              </FlipCard>
             </Col>
             <Col xs={3}>
 
