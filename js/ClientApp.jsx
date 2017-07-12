8e20f26658cae0c43174e97da90c4d3f83fdbb02
kndllbrwn <kndllbrwn1914@gmail.com>
2017-07-11 22:37:25 -0400

    Updated mobile navigation

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -21,50 +21,50 @@ const App = React.createClass({
   render() {
     return (
       <Grid fluid>
-        <div className="app">
-          <BrowserRouter>
-            <nav className="nav">
-              <Link to="/">
-                <i className="fa fa-history" />
-                <br/>
-                <span className="nav__span">Experience</span>
-              </Link>
-              <Link to="/examples">
-                <i className="fa fa-code" />
-                <br/>
-                <span className="nav__span">Examples</span>
-              </Link>
-              <Link to="/education">
-                <i className="fa fa-graduation-cap" />
-                <br/>
-                <span className="nav__span">Education</span>
-              </Link>
-              <Link to="/skills">
-                <i className="fa fa-certificate" />
-                <br/>
-                <span className="nav__span">Skills</span>
-              </Link>
-              <Link to="/interests">
-                <i className="fa fa-puzzle-piece" />
-                <br/>
-                <span className="nav__span">Interests</span>
-              </Link>
-            </nav>
-            <Row>
-              <Col xs={12}>
-                <Header />
-                <Bio />
-              </Col>
-            </Row>
-            <Switch>
-              <Route exact path="/" component={props => <Experience jobs={preload.jobs} {...props} />} />
-              <Route path="/examples" component={props => <Examples projects={preload.projects} {...props} />} />
-              <Route path="/education" component={Education} />
-              <Route path="/skills" component={props => <Skills skills={preload.skills} {...props} />} />
-              <Route path="/interests" component={Interests} />
-            </Switch>
-          </BrowserRouter>
-        </div>
+        <BrowserRouter>
+          <div className="app">
+              <nav className="nav">
+                <Link to="/">
+                  <i className="fa fa-history" />
+                  <br/>
+                  <span className="nav__span">Experience</span>
+                </Link>
+                <Link to="/examples">
+                  <i className="fa fa-code" />
+                  <br/>
+                  <span className="nav__span">Examples</span>
+                </Link>
+                <Link to="/education">
+                  <i className="fa fa-graduation-cap" />
+                  <br/>
+                  <span className="nav__span">Education</span>
+                </Link>
+                <Link to="/skills">
+                  <i className="fa fa-certificate" />
+                  <br/>
+                  <span className="nav__span">Skills</span>
+                </Link>
+                <Link to="/interests">
+                  <i className="fa fa-puzzle-piece" />
+                  <br/>
+                  <span className="nav__span">Interests</span>
+                </Link>
+              </nav>
+              <Row>
+                <Col xs={12}>
+                  <Header />
+                  <Bio />
+                </Col>
+              </Row>
+              <Switch>
+                <Route exact path="/" component={props => <Experience jobs={preload.jobs} {...props} />} />
+                <Route path="/examples" component={props => <Examples projects={preload.projects} {...props} />} />
+                <Route path="/education" component={Education} />
+                <Route path="/skills" component={props => <Skills skills={preload.skills} {...props} />} />
+                <Route path="/interests" component={Interests} />
+              </Switch>
+          </div>
+        </BrowserRouter>
       </Grid>
     );
   }
