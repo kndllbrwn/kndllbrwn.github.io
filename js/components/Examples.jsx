02231c3636fb0f2ce56bc0de90a375b33a05abf7
Kendall Brown <kndllbrwn1914@gmail.com>
2020-04-13 05:45:14 -0400

    Updated projects header

--- js/components/Examples.jsx
+++ js/components/Examples.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import Title from './Title';
+import React from "react";
+import Title from "./Title";
 
 const Examples = React.createClass({
   render() {
     const { title } = this.props;
     return (
       <div className="componentBody">
-        <Title header="Examples" />
+        <Title header="Projects" />
         <div id="examples">
-          {this.props.projects.map(project => {
+          {this.props.projects.map((project) => {
             return (
               <div key={project.title}>
-                <h3 className="project__title">
-                  {project.title}
-                </h3>
+                <h3 className="project__title">{project.title}</h3>
                 <a target="_blank" href={project.link}>
-                  <img className="projectImage" src={`/public/images/${project.image}.png`} />
+                  <img
+                    className="projectImage"
+                    src={`/public/images/${project.image}.png`}
+                  />
                 </a>
-                <p>
-                  {project.description}
-                </p>
+                <p>{project.description}</p>
               </div>
             );
           })}
         </div>
       </div>
     );
-  }
+  },
 });
 
 export default Examples;
